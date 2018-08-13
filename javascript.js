b8925391956690a4ac0e805e3109003e07e93b53
cefimenda <jeffymenda@gmail.com>
2018-08-13 16:31:06 -0400

    re-designed

---
+++ javascript.js
@@ -0,0 +1,40 @@
+$(function () {
+    $(".carousel").carousel({
+        interval: false
+    })
+    $(".portfolioLearnMore").click(function () {
+        $(".myPortfolio").show()
+        $(".aboutMe").hide()
+        $(".contactMe").hide()
+
+    })
+    $(".aboutMeLearnMore").click(function () {
+        $(".myPortfolio").hide()
+        $(".aboutMe").show()
+        $(".contactMe").hide()
+
+    })
+    $(".contactMeLearnMore").click(function () {
+        $(".myPortfolio").hide()
+        $(".aboutMe").hide()
+        $(".contactMe").show()
+    })
+    $(".navbarAbout").click(function(){
+        $(".aboutMeLearnMore").click()
+    })
+    $(".navbarPortfolio").click(function(){
+        $(".portfolioLearnMore").click()
+    })    
+    $(".navbarContact").click(function(){
+        $(".contactMeLearnMore").click()
+    })
+    $(".learnMoreButtons").click(function(){
+        setTimeout(function(){
+            window.scroll({
+                top:400,
+                behavior:'smooth'
+            })
+        },100)
+    })
+})
+
