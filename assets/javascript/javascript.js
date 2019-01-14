a69724d68ca1e44280b32e2f90eb911412e0af4d
cefimenda <jeffymenda@gmail.com>
2019-01-14 17:52:08 -0500

    changed projects page dynamics to make it simpler and cleaner

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -34,52 +34,55 @@ $(function () {
             })
         }, 100)
     })
-    $(".nav-link.bg-blue").click(function () {
-        if (!page.animating) {
-            page.animating = true
-            $(".scrollRightInfo").fadeOut()
-            var pillsList = $(".pills")
-            for (var i = 0; i < pillsList.length; i++) {
-                $(pillsList[i]).removeClass("active")
-            }
-            $(this).addClass('active')
-            //disable scrolling when animation starts
-            $(".projectsRow").css({
-                'overflow-x': 'hidden'
-            })
-
-            var cover = $(".cover")
-            $(".projectsRow")[0].scroll({
-                left: 0,
-                behavior: "smooth"
-            })
-            cover.animate({
-                width: "100%",
-                padding: "100%"
-            }, 1500, "swing")
-
-            var target = $(this).text().trim().split(" ")[0].toLowerCase().trim()
-            setTimeout(function () {
-                createProjectCards(target)
-            }, 700)
-
-            $(".cover").animate({
-                width: "0%",
-                padding: "1px"
-            }, 1500, "swing")
-            //enable scrolling when animation ends
-            setTimeout(function () {
-                $(".projectsRow").css({
-                    'overflow-x': 'scroll'
-                })
-                console.log($(".projectsDump").css('width'))
-                console.log($(".projectsRow").css('width'))
-                if (Number($(".projectsDump").css('width').split("px")[0]) >= Number($(".projectsRow").css('width').split("px")[0])) {
-                    $(".scrollRightInfo").fadeIn()
-                }
-                page.animating = false
-            }, 3000)
+    $(".pillButton").click(function () {
+        var target = $(this).text().trim().split(" ")[0].toLowerCase().trim();
+        createProjectCards(target);
+        var pillsList = $(".pillButton")
+        for (var i = 0; i < pillsList.length; i++) {
+            $(pillsList[i]).removeClass("activePill")
         }
+        $(this).addClass('activePill')
+        // if (!page.animating) {
+        //     page.animating = true
+        //     $(".scrollRightInfo").fadeOut()
+        //     
+        //     //disable scrolling when animation starts
+        //     $(".projectsRow").css({
+        //         'overflow-x': 'hidden'
+        //     })
+
+        //     var cover = $(".cover")
+        //     $(".projectsRow")[0].scroll({
+        //         left: 0,
+        //         behavior: "smooth"
+        //     })
+        //     cover.animate({
+        //         width: "100%",
+        //         padding: "100%"
+        //     }, 1500, "swing")
+
+        //     var target = $(this).text().trim().split(" ")[0].toLowerCase().trim()
+        //     setTimeout(function () {
+        //         createProjectCards(target)
+        //     }, 700)
+
+        //     $(".cover").animate({
+        //         width: "0%",
+        //         padding: "1px"
+        //     }, 1500, "swing")
+        //     //enable scrolling when animation ends
+        //     setTimeout(function () {
+        //         $(".projectsRow").css({
+        //             'overflow-x': 'scroll'
+        //         })
+        //         console.log($(".projectsDump").css('width'))
+        //         console.log($(".projectsRow").css('width'))
+        //         if (Number($(".projectsDump").css('width').split("px")[0]) >= Number($(".projectsRow").css('width').split("px")[0])) {
+        //             $(".scrollRightInfo").fadeIn()
+        //         }
+        //         page.animating = false
+        //     }, 3000)
+        // }
     })
     $("#swipe").swipe({
         allowPageScroll: "horizontal"
@@ -90,11 +93,11 @@ var page = {
 }
 var projects = {
 
-    projections:{
-        name:"Projections Builder",
-        link:"https://docs.google.com/spreadsheets/d/1Xjq8KEABuv9WVhshuWG8TGJLglfgXg9oJEJV_Q-548M/edit?usp=sharing",
-        imageRef:"assets/images/Projections.png",
-        type:"tools"
+    projections: {
+        name: "Projections Builder",
+        link: "https://docs.google.com/spreadsheets/d/1Xjq8KEABuv9WVhshuWG8TGJLglfgXg9oJEJV_Q-548M/edit?usp=sharing",
+        imageRef: "assets/images/Projections.png",
+        type: "tools"
     },
     liri: {
         name: "LIRI",
@@ -127,12 +130,12 @@ var projects = {
         imageRef: "assets/images/scavenger.png",
         type: "websites"
     },
-    helpFinder:{
-        name:"Help Finder",
-        link:"https://help-finder.herokuapp.com/",
-        github:"https://github.com/cefimenda/help-finder",
-        imageRef:"assets/images/helpFinder.png",
-        type:"websites"
+    helpFinder: {
+        name: "Help Finder",
+        link: "https://help-finder.herokuapp.com/",
+        github: "https://github.com/cefimenda/help-finder",
+        imageRef: "assets/images/helpFinder.png",
+        type: "websites"
     },
     // giphySearch: {
     //     name: "Giphy Search",
@@ -173,86 +176,91 @@ var projects = {
 }
 
 function createProjectCards(type) {
-    $(".startupsDump").empty()
-    $(".websitesDump").empty()
-    $(".gamesDump").empty()
-    $(".projectsDump").empty()
-    for (var id in projects) {
-        if (projects[id].type == type) {
-            var targetRow = $(".projectsDump")
-            var col = $("<div>").addClass("mb-3 mt-3 mx-4 p-0")
-            targetRow.append(col)
-            var card = $("<div>").addClass("card shadow text-center mr-3")
-            card.css({
-                "width": "18rem",
-                "height": "250.172px"
-            })
-            col.append(card)
-            var img = $("<img>").addClass("card-img-top")
-            img.attr("src", projects[id].imageRef)
-            img.css({
-                "height":"150.172px"
-            })
-            card.append(img)
-            var body = $("<div>").addClass("card-body")
-            card.append(body)
-            var title = $("<h5>").addClass("card-title")
-            body.append(title)
+    var targetRow = $(".projectsDisplay")
+    targetRow.fadeOut()
+    targetRow.empty()
+    setTimeout(function () {
+        for (var id in projects) {
+            if (projects[id].type == type) {
+                var col = $("<div>").addClass("col-4 mb-3")
+                col.css({
+                    "width": "18rem",
+                    "height": "250.172px",
+                    "max-width": "18rem",
+                    "max-height": "250.172px"
+                })
+                targetRow.append(col)
+                var card = $("<div>").addClass("card shadow text-center")
 
-            var link = $("<a>").attr("href", projects[id].link)
-            link.addClass("project-title")
-            link.attr("target", "_blank")
-            link.text(projects[id].name)
-            title.append(link)
+                col.append(card)
+                var img = $("<img>").addClass("card-img-top")
+                img.attr("src", projects[id].imageRef)
+                img.css({
+                    "height": "150.172px"
+                })
+                card.append(img)
+                var body = $("<div>").addClass("card-body")
+                card.append(body)
+                var title = $("<h5>").addClass("card-title")
+                body.append(title)
 
-            if (projects[id].link && projects[id].github) {
-                var symbolRow = $("<div>").addClass("row")
-                card.append(symbolRow)
+                var link = $("<a>").attr("href", projects[id].link)
+                link.addClass("project-title")
+                link.attr("target", "_blank")
+                link.text(projects[id].name)
+                title.append(link)
 
-                var viewCol = $("<div>").addClass("col-6 pl-5 border-right")
-                symbolRow.append(viewCol)
-                var gitCol = $("<div>").addClass("col-6 pr-5")
-                symbolRow.append(gitCol)
+                if (projects[id].link && projects[id].github) {
+                    var symbolRow = $("<div>").addClass("row")
+                    card.append(symbolRow)
 
-                var viewAnc = $("<a>").attr("href", projects[id].link)
-                viewAnc.attr("target", "_blank")
-                viewCol.append(viewAnc)
-                var viewImg = $("<i>").addClass("fas fa-eye fa-lg")
-                viewAnc.append(viewImg)
+                    var viewCol = $("<div>").addClass("col-6 pl-5 border-right")
+                    symbolRow.append(viewCol)
+                    var gitCol = $("<div>").addClass("col-6 pr-5")
+                    symbolRow.append(gitCol)
 
-                var gitAnc = $("<a>").attr("href", projects[id].github)
-                gitAnc.attr("target", "_blank")
-                gitCol.append(gitAnc)
-                var gitImg = $("<i>").addClass("fab fa-github fa-lg")
-                gitAnc.append(gitImg)
+                    var viewAnc = $("<a>").attr("href", projects[id].link)
+                    viewAnc.attr("target", "_blank")
+                    viewCol.append(viewAnc)
+                    var viewImg = $("<i>").addClass("fas fa-eye fa-lg")
+                    viewAnc.append(viewImg)
+
+                    var gitAnc = $("<a>").attr("href", projects[id].github)
+                    gitAnc.attr("target", "_blank")
+                    gitCol.append(gitAnc)
+                    var gitImg = $("<i>").addClass("fab fa-github fa-lg")
+                    gitAnc.append(gitImg)
+                }
+                else if (projects[id].link) {
+                    var symbolRow = $("<div>").addClass("row")
+                    card.append(symbolRow)
+
+                    var viewCol = $("<div>").addClass("col-12")
+                    symbolRow.append(viewCol)
+
+                    var viewAnc = $("<a>").attr("href", projects[id].link)
+                    viewAnc.attr("target", "_blank")
+                    viewCol.append(viewAnc)
+                    var viewImg = $("<i>").addClass("fas fa-eye fa-lg")
+                    viewAnc.append(viewImg)
+                }
+                else if (projects[id].github) {
+                    var symbolRow = $("<div>").addClass("row")
+                    card.append(symbolRow)
+
+                    var gitCol = $("<div>").addClass("col-12")
+                    symbolRow.append(gitCol)
+
+                    var gitAnc = $("<a>").attr("href", projects[id].github)
+                    gitAnc.attr("target", "_blank")
+                    gitCol.append(gitAnc)
+                    var gitImg = $("<i>").addClass("fab fa-github fa-lg")
+                    gitAnc.append(gitImg)
+                }
             }
-            else if (projects[id].link) {
-                var symbolRow = $("<div>").addClass("row")
-                card.append(symbolRow)
-
-                var viewCol = $("<div>").addClass("col-12")
-                symbolRow.append(viewCol)
-
-                var viewAnc = $("<a>").attr("href", projects[id].link)
-                viewAnc.attr("target", "_blank")
-                viewCol.append(viewAnc)
-                var viewImg = $("<i>").addClass("fas fa-eye fa-lg")
-                viewAnc.append(viewImg)
-            }
-            else if (projects[id].github) {
-                var symbolRow = $("<div>").addClass("row")
-                card.append(symbolRow)
-
-                var gitCol = $("<div>").addClass("col-12")
-                symbolRow.append(gitCol)
-
-                var gitAnc = $("<a>").attr("href", projects[id].github)
-                gitAnc.attr("target", "_blank")
-                gitCol.append(gitAnc)
-                var gitImg = $("<i>").addClass("fab fa-github fa-lg")
-                gitAnc.append(gitImg)
-            }
-
         }
-    }
+        targetRow.fadeIn()
+    }, 500)
+
+
 }
